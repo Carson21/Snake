b155be6aa6caf90b75b237b6eff9213074eef542
Carson Keavney <carsonkeavney@outlook.com>
2022-10-14 01:42:55 -0400

    Snake can't go back into itself

--- js/input.js
+++ js/input.js
@@ -1,23 +1,29 @@
 let inputDirection = { x: 0, y: 0 }
+let lastDir = { x: 0, y: 0 }
 
 window.addEventListener("keydown", (e) => {
     console.log(e.key)
     switch (e.key) {
         case "ArrowUp":
+            if (lastDir.y !== 0) break
             inputDirection = { x: 0, y: -1 }
             break
         case "ArrowDown":
+            if (lastDir.y !== 0) break
             inputDirection = { x: 0, y: 1 }
             break
         case "ArrowRight":
+            if (lastDir.x !== 0) break
             inputDirection = { x: 1, y: 0 }
             break
         case "ArrowLeft":
+            if (lastDir.x !== 0) break
             inputDirection = { x: -1, y: 0 }
             break
     }
 })
 
 export const getInputDirection = () => {
+    lastDir = inputDirection
     return inputDirection
 }
